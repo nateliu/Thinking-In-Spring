f3d58710e68dae26027dc617abcdabc8f05c6177
Nate Liu <jinliangliu@163.com>
2016-10-04 21:21:22 +0800

    use MockMvc to do UnitTest

---
+++ Spring-MVC/src/test/java/org/springframework/samples/HomeControllerTest.java
@@ -0,0 +1,22 @@
+package org.springframework.samples;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+/**
+ * Created by Administrator on 2016/10/4 0004.
+ */
+public class HomeControllerTest {
+    @Test
+    public void homeTest() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+        mockMvc.perform(get("/")).andExpect(view().name("home"));
+    }
+
+}
